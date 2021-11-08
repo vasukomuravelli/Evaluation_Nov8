9095eefdd270b65940bd7e39994a6ab764948a7c
vasukomuravelli <vasukomuravelli96@gmail.com>
2021-11-08 14:15:33 +0530

    Create navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return `
+    <div class="navbar">
+        <p>Menu</p>
+        <div class = "user">
+            <p>Signup</p>
+            <p>Login</p>
+        </div>
+    </div>`
+}
+export default navbar;
